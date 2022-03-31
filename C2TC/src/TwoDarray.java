fe4da24d6a90b5997309e6a99450adc27a04a8ca
ashwini <ashwini@DESKTOP-JTE97RK>
2022-03-31 22:55:24 +0530

    first code

---
+++ C2TC/src/TwoDarray.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class TwoDarray {
+
+	public static void main(String[] args) {
+		int a[][]= {{1,2,3,4},{2,4,6,8}};
+		for (int i=0;i<a.length;i++)
+		{
+			for (int j=0;j<a[i].length;j++)
+			{
+				System.out.print(a[i][j]);
+				
+			}
+			
+			system.out,print('\n')
+		}
+
+	}
+
+}
